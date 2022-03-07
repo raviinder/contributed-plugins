bc2d5b40a0f8e7682dde18dcce7158d8a676a4dd
raviinder <raviinder@gmail.com>
2022-03-07 08:39:42 -0500

    Updates

--- chart/common.d.ts
+++ chart/common.d.ts
@@ -1,9 +1,15 @@
-export default class Constants {
+export default class Common {
     static _panelOptionsShrink: object;
     static _panelOptionsExpand: {
         'margin-top': string;
         'margin-bottom': string;
         'margin-right': string;
         'margin-left': string;
+    };
+    static _controlIdsOrClass: {
+        btnExpendChartPaneID: string;
+    };
+    static constants: {
+        rotateCssClass: string;
     };
 }
